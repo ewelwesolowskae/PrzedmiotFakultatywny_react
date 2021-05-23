d52b7af991977909f9bdba722ee720342022c12c
dawidszczepkowski <46352482+dawidszczepkowski@users.noreply.github.com>
2021-05-24 01:04:47 +0200

    17102021
    notatki z wykladu z dnia 17 10 2021

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -6,12 +6,12 @@ import http from '../../utils/http';
 import movieService from '../../services/movies.service';
 const Home = () =>{
     const [search, SetSearch] = React.useState('harry potter');
-    const [movie,setMovie] = React.useState<any>({});
+    const [movie,setMovie] = React.useState({});
 
     React.useEffect(()=>{
         const makeRequest = async () => {
         try{
-            const response = movieService.searchByName(search,1);
+            const response = movieService.searchByName(search, 1);
     //`http://www.omdbapi.com/?apikey=${omdApiKey}&s=${search}&plot=full`
 
     //const response = await <any>http.get('http://www.omdbapi.com/', {
